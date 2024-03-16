6c73dd5faf77641beb6b890d5998d72e648b773f
jat121 <jatinpal4014@gmail.com>
2024-03-16 23:25:29 +0530

    desktop added

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ fetch("./flags.json")
             .then((data) => {
                 userCountry = data.country_name;
                 const flagElement = document.getElementById("user-flag");
+                const flagMobileElement = document.getElementById("user-flag-mobile");
                 const countrynameElement = document.getElementById("user-country");
+                const countrynameMobileElement = document.getElementById("user-country-mobile");
                 countrynameElement.innerText = userCountry ?? "London";
+                countrynameMobileElement.innerText = userCountry ?? "London";
                 flagElement.src = countriesFlags[userCountry]?.mini ?? "";		// for small sized flag
+                flagMobileElement.src = countriesFlags[userCountry]?.mini ?? "";		// for small sized flag
             });
     });
 
