1545693db4bb43eb6044f44cc73ea760371b0efd
jat121 <jatinpal4014@gmail.com>
2024-03-16 17:50:43 +0530

    added flags and countries

---
+++ index.js
@@ -0,0 +1,17 @@
+let countriesFlags = {};
+let userCountry = "";
+fetch("./flags.json")
+  .then((res) => res.json())
+  .then((data) => {
+    countriesFlags = data;
+    fetch("http://ip-api.com/json")
+      .then((res) => res.json())
+      .then((data) => {
+        userCountry = data.country;
+
+        const flagElement = document.getElementById("user-flag");
+        const countrynameElement = document.getElementById("user-country");
+        countrynameElement.innerText = userCountry;
+        flagElement.src = countriesFlags[userCountry].mini;		// for small sized flag
+      });
+  });
