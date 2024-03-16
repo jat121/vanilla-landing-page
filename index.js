7edc7c20e3f0795ef08076040767f8fd57a5bac7
jat121 <jatinpal4014@gmail.com>
2024-03-16 20:20:58 +0530

    fix: api

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 let countriesFlags = {};
 let userCountry = "";
 fetch("./flags.json")
-  .then((res) => res.json())
-  .then((data) => {
-    countriesFlags = data;
-    fetch("http://ip-api.com/json")
-      .then((res) => res.json())
-      .then((data) => {
-        userCountry = data.country;
-        const flagElement = document.getElementById("user-flag");
-        const countrynameElement = document.getElementById("user-country");
-        countrynameElement.innerText = userCountry ?? "London";
-        flagElement.src = countriesFlags[userCountry]?.mini ?? "";		// for small sized flag
-      });
-  });
+    .then((res) => res.json())
+    .then((data) => {
+        countriesFlags = data;
+        console.log(countriesFlags)
+        fetch("https://ipapi.co/json/")
+            .then((res) => res.json())
+            .then((data) => {
+        debugger;
+                userCountry = data.country_name;
+                const flagElement = document.getElementById("user-flag");
+                const countrynameElement = document.getElementById("user-country");
+                countrynameElement.innerText = userCountry ?? "London";
+                flagElement.src = countriesFlags[userCountry]?.mini ?? "";		// for small sized flag
+            });
+    });
+
+
+
+
