06372d88bdfc9e97b22fe5213bff229203b72332
jat121 <jatinpal4014@gmail.com>
2024-03-20 00:08:29 +0530

    added am pm time

--- chat.js
+++ chat.js
@@ -97,7 +97,7 @@ function appendMessage(name, img, side, text) {
   function formatAMPM(date) {
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let ampm = hours >= 12 ? 'pm' : 'am';
+    let ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12;
     minutes = minutes < 10 ? '0'+minutes : minutes;
