9c04ca48fbdfc0ca442e80d7b9072e1a9c0240d3
jat121 <jatinpal4014@gmail.com>
2024-03-19 23:53:53 +0530

    added am pm time

--- chat.js
+++ chat.js
@@ -37,15 +37,17 @@ function appendMessage(name, img, side, text) {
     const msgHTML = `
       <div class="msg ${side}-msg">
         <div class="msg-img" style="background-image: url(${img})"></div>
-  
+        <div>
         <div class="msg-bubble">
         <div class="msg-text">${text}</div>
 
           <div class="msg-info">
           <!-- <div class="msg-info-name">${name}</div> -->
-           <div class="msg-info-time">${formatDate(new Date())}</div> 
+          
           </div>
   
+        </div>
+        <div class="msg-info-time">${formatAMPM(new Date())}</div> 
         </div>
       </div>
     `;
@@ -57,7 +59,7 @@ function appendMessage(name, img, side, text) {
         <div class="msg-bubble-desk">
           <div class="msg-info-desk">
             <div class="msg-info-name-desk">${name}</div>
-            <div class="msg-info-time-desk">${formatDate(new Date())}</div>
+            <div class="msg-info-time-desk">${formatAMPM(new Date())}</div>
           </div>
   
           <div class="msg-text-desk">${text}</div>
@@ -90,6 +92,17 @@ function appendMessage(name, img, side, text) {
     const m = "0" + date.getMinutes();
   
     return `${h.slice(-2)}:${m.slice(-2)}`;
+  }
+
+  function formatAMPM(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
   }
   
   function random(min, max) {
